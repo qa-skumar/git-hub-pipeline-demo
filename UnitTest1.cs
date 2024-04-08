4065b466839d52656a21ecd38ac4e2da1fa30539
qa-skumar <sushant.kumar@bjss.com>
2024-04-08 18:05:09 +0100

    Run test on ff

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,8 +10,9 @@ namespace PlaywrightTests;
 [TestFixture]
 public class Tests : PageTest
 {
+    
     [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    public async Task Test1()
     {
         await Page.GotoAsync("https://playwright.dev");
 
@@ -29,9 +30,17 @@ public class Tests : PageTest
 
         // Expects the URL to contain intro.
         await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+        await Page.Locator("//*[@class='navbar__link']").ClickAsync();
+        Thread.Sleep(1000);
+        await Page.Locator("//*[@class='dropdown__menu']//li").Nth(1).HighlightAsync();
+        Thread.Sleep(2000);
+        await Page.Locator("//*[@class='dropdown__menu']//li").Nth(1).ClickAsync();
+        Thread.Sleep(2000);
+      
     }
+      
     [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage1()
+     public async Task Test2()
     {
         await Page.GotoAsync("https://playwright.dev");
 
@@ -49,62 +58,14 @@ public class Tests : PageTest
 
         // Expects the URL to contain intro.
         await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+        await Page.Locator("//*[@class='navbar__link']").ClickAsync();
+         await Page.Locator("//*[@class='dropdown__menu']//li").Nth(2).HighlightAsync();
+        Thread.Sleep(2000);
+        await Page.Locator("//*[@class='dropdown__menu']//li").Nth(2).ClickAsync();
+        Thread.Sleep(2000);
+      
     }
-    [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage2()
-    {
-        await Page.GotoAsync("https://playwright.dev");
-
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
-
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-        // Click the get started link.
-        await getStarted.ClickAsync();
-
-        // Expects the URL to contain intro.
-        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
-    }
-    [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage3()
-    {
-        await Page.GotoAsync("https://playwright.dev");
-
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
-
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-        // Click the get started link.
-        await getStarted.ClickAsync();
-
-        // Expects the URL to contain intro.
-        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
-    }
-     [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage4()
-    {
-        await Page.GotoAsync("https://playwright.dev");
-
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
-
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-       
-    }
+   
+   
     
 }
