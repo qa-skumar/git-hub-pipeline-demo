472b7c95aa7bf9ceb6d7b0790c47fd6739881d28
qa-skumar <skumarqe25@gmail.com>
2024-04-28 20:36:56 +0100

    Run tests on FF

--- obj/Debug/net6.0/PlaywrightTests.AssemblyInfo.cs
+++ obj/Debug/net6.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5e75133e58a63ce637d325314419af0bf0c226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0a470af2652af47893f6a758fb378f7b9cbb5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
