5e0716a5b2be37b0f8eaeca54b868f58e9e8a81b
qa-skumar <skumarqe25@gmail.com>
2024-04-29 06:59:49 +0100

    Run test on pipeline

--- obj/Debug/net6.0/PlaywrightTests.AssemblyInfo.cs
+++ obj/Debug/net6.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c956e1ab1e9cfd6f0402586185ec858554d955")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bceb8d0b43d45a814509057b1daeff126c4d3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
