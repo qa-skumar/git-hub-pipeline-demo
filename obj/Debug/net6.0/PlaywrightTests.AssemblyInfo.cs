21b33edfbc16e353f4dc569e271113c0fd87f63d
qa-skumar <skumarqe25@gmail.com>
2024-04-30 08:48:00 +0100

    Update broeswe

--- obj/Debug/net6.0/PlaywrightTests.AssemblyInfo.cs
+++ obj/Debug/net6.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0716a5b2be37b0f8eaeca54b868f58e9e8a81b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5803c4b3af3541359d00d513688cdc68c4a3e35e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
